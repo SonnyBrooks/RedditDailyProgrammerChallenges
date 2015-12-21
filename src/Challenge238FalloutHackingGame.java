0f7defcf5f7929452a0b7248b28377fe88d5e977
Andrew Budziszek <abudziszek@gmail.com>
2015-12-21 11:02:05 -0600

    Challenge 238: Fallout Hacking Game
    SBUtil Update to add File IO components.
    Added an ArrayList copy function using Generics.
    Added toHexString(int).
    
    Added “Files” directory.
    Added dictionary ‘enable1.txt’ to Files directory.

---
+++ src/Challenge238FalloutHackingGame.java
@@ -0,0 +1,130 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+/**
+ * Created by Andrew Budziszek on 12/20/15.
+ * Reddit Daily Programmer: Challenge #238
+ * Fallout Hacking Game[INTERMEDIATE]
+ */
+
+public class Challenge238FalloutHackingGame {
+    public static SBUtil util = new SBUtil("/Users/Andrew/IdeaProjects/RedditDailyProgrammerChallenges/src/Files/enable1.txt");
+    private static ArrayList<String> Words = new ArrayList<>();
+    private static String secretPhrase = "";
+    private static String Board = "";
+    private static int triesLeft = -1;
+
+    public static void main(String[] args){
+
+        while(true){
+            int difficulty = util.promptInt("Enter Difficulty(1-5): ");
+            setupGame(difficulty);
+            playGame();
+        }
+    }
+
+    private static void setupGame(int difficulty){
+        Words = null;
+        util = new SBUtil("/Users/Andrew/IdeaProjects/RedditDailyProgrammerChallenges/src/Files/enable1.txt");
+        int lengthOfWords = -1;
+        switch (difficulty){
+            case 1:
+                lengthOfWords = 4;
+                break;
+            case 2:
+                lengthOfWords = 5;
+                break;
+            case 3:
+                lengthOfWords = 6;
+                break;
+            case 4:
+                lengthOfWords = 7;
+                break;
+            case 5:
+                lengthOfWords = 9;
+                break;
+            default:
+                lengthOfWords = 5;
+                break;
+        }
+        try {
+            String word = "";
+            Words = new ArrayList<>();
+            while ((word = util.br.readLine()) != null) {
+                if(word.length() == lengthOfWords)
+                    Words.add(word.toUpperCase());
+            }
+            Collections.shuffle(Words);
+            Words = new ArrayList<>(Words.subList(0, 10));
+            triesLeft = 4;
+            secretPhrase = Words.get(new Random().nextInt(Words.size()));
+            Board = makeBoard();
+        }catch(Exception e){
+            util.err("Error in setupGame()... \n" + e.toString());
+        }
+    }
+
+    private static void playGame(){
+        while(triesLeft != 0){
+            util.printl(triesRemaining());
+            util.printl(Board);
+            String guess = util.promptString("> Enter guess: ").toUpperCase();
+            util.printl("> " + guess);
+            if(guess.equals(secretPhrase)){
+                util.printl("> EXACT MATCH!\nAccess granted!");
+                break;
+            }
+            int numCharsCorrect = 0;
+            if(guess.length() == secretPhrase.length()) {
+                for (int i = 0; i < secretPhrase.length(); ++i) {
+                    if (secretPhrase.charAt(i) == guess.charAt(i))
+                        numCharsCorrect++;
+                }
+            }
+            util.printl("> ACCESS DENIED!");
+            util.printl("> " + numCharsCorrect + "/" + secretPhrase.length() + " correct...");
+            triesLeft--;
+        }
+        if(triesLeft == 0)
+            util.printl("> The secret phrase was " + secretPhrase + "\n");
+    }
+
+    private static String makeBoard(){
+        String board = "";
+        int i = 0;
+        while(!Words.isEmpty()){
+            //First Column
+            board += util.toHexString(i) + " " + formatColumns(Words.remove(0)) + " ";
+            //Second Column
+            board += " " + util.toHexString(i + 15) + " " + formatColumns(Words.remove(0)) + "\n";
+            //Random Char Rows
+            board += util.toHexString(i + 30) + " " + formatColumns("") + "  ";
+            board += util.toHexString(i + 35) + " " + formatColumns("") + "\n";
+            i++;
+        }
+        return board;
+    }
+
+    private static String triesRemaining(){
+        String tries = String.format("%d ATTEMPT(S) REMAINING: ", triesLeft);
+        for(int i = 0; i < triesLeft; ++i)
+            tries += "# ";
+        return tries + "\n";
+    }
+
+    public static String formatColumns(String board){
+        String formatted = "";
+        char[] miscChars = {'%', '[', '(', ')','/','@','^',']',';', '>', '<'};
+        int firstChar = (15 - board.length())/2;
+        for(int i = 0; i < firstChar; i++){
+            formatted += miscChars[new Random().nextInt(miscChars.length)];
+        }
+        formatted += board;
+        while(formatted.length() < 15){
+            formatted += miscChars[new Random().nextInt(miscChars.length)];
+        }
+
+        return formatted;
+    }
+}
--- src/SBUtil.java
+++ src/SBUtil.java
@@ -1,20 +1,41 @@
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
  * Created by Andrew on 12/3/15.
  * Sonny Brooks Utilities
  */
-public class SBUtil {
+public class SBUtil <T>{
 
     Scanner stdIn;
+    FileReader fr;
+    BufferedReader br;
 
     public SBUtil(){
+        this("");
+    }
+    public SBUtil(String filePath){
         this.stdIn = new Scanner(System.in);
+        if(!filePath.equals(""))
+            fr = getFileReader(filePath);
+        if(fr != null)
+            br = new BufferedReader(fr);
     }
 
     public void printl(Object x){
         System.out.println(x.toString());
     }
+
+    public void print(Object x){
+        System.out.print(x.toString());
+    }
+
+    public void err(Object x){System.err.println(x);}
 
     public void goodbye(){this.printl("Goodbye!");}
 
@@ -24,18 +45,42 @@ public class SBUtil {
 
     private double getDouble(){return stdIn.nextDouble();}
 
+    private FileReader getFileReader(String filePath){
+        try {
+            return new FileReader(filePath);
+        }catch(IOException ioE){
+            return null;
+        }
+    }
+
     public String promptString(String message){
-        this.printl(message);
+        this.print(message);
         return this.getString();
     }
 
     public int promptInt(String message){
-        this.printl(message);
+        this.print(message);
         return this.getInt();
     }
-    public double promptDouble(String message){
-        this.printl(message);
+    public double promptDouble(String message) {
+        this.print(message);
         return this.getDouble();
+    }
+
+    public ArrayList<T> copyArray(ArrayList<T> arr){
+        ArrayList<T> copy = new ArrayList<>();
+        for(T o : arr){
+            copy.add(o);
+        }
+        return copy;
+    }
+
+    public static String toHexString(int n){
+        String sNum = Integer.toHexString(3000 + (int)Math.pow(n, 3));
+        while(sNum.length() < 4){
+            sNum += "0";
+        }
+        return "0x" + sNum.toUpperCase();
     }
 
 }
